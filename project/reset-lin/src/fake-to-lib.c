0f6b838c434f974402d3a9b9c5381f6b4e0e4ba4
gnsyxiang <gnsyxiang@163.com>
2018-12-02 21:16:29 +0800

    feat: add reset pin for lin-boss

---
+++ project/reset-lin/src/fake-to-lib.c
@@ -0,0 +1,26 @@
+/**
+ * 
+ * Release under GPLv2.
+ * 
+ * @file    fake-to-lib.c
+ * @brief   
+ * @author  gnsyxiang <gnsyxiang@163.com>
+ * @date    30/10 2018 14:40
+ * @version v0.0.1
+ * 
+ * @since    note
+ * @note     note
+ * 
+ *     change log:
+ *     NO.     Author              Date            Modified
+ *     00      zhenquan.qiu        30/10 2018      create the file
+ * 
+ *     last modified: 30/10 2018 14:40
+ */
+#include <stdio.h>
+
+void fake_dis_info(void)
+{
+    printf("hello fake info \n");
+}
+
